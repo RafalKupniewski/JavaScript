b7f3949638e9d89c08165c333b04d2df2909fe7c
rkupniewski <rkupniewski@sigma.inf.ug.edu.pl>
2017-03-25 22:47:34 +0100

    zad2

---
+++ Zad2/Iphon.js
@@ -0,0 +1,60 @@
+
+class Iphon {
+    constructor(json){
+    //let self = this;
+    this.model = json.model;
+    this.procesor = json.procesor;
+    this.aparat = json.aparat;
+    this.pojemnosc = json.pojemnosc;
+	}
+    toTableRow = function(){
+		return `<tr><td>
+		${this.model}
+		</td><td>
+		${this.procesor}
+		</td><td>
+        ${this.aparat}
+		</td><td>
+        ${this.pojemnosc}
+		</tr>`;
+	}
+}
+
+class ListaIphonow{
+	constructor (){
+    this.iphony = [];
+	}
+
+    dodajIphona(json) {
+		this.iphony.push(new Iphon(json));
+	}
+
+    toTable = function() {
+		let table = '<table><tr><th>Tytul</th> <th>Gatunek</th> <th>Ocena</th> <th>Studio</th></tr>';
+		for(let i=0; i<this.iphony.length; i++) {
+				table += this.iphony[i].toTableRow();
+		}
+		table += '</table>';
+		return table;
+	}
+}
+ 
+
+
+function init(){
+	let plik = new XMLHttpRequest();
+plik.onload = reqListener;
+plik.open("get", "./bazka.json", true);
+plik.send();
+let json;
+
+function reqListener(e) {
+	json = JSON.parse(this.responseText);
+   	ListaIphonow = new ListaIphonow();
+	for(let i=0;json.length>i;i++){
+    	ListaIphonow.dodajIphona(json[i]);
+    }
+    //let context = document.getElementById('table');
+	context.innerHTML = ListaIphonow.toTable();
+}
+
